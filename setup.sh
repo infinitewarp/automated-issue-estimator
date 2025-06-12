79c10a20ca32b8b0410f567a6da177a256737b7c
Brad Smith <brasmith@redhat.com>
2025-06-12 17:03:50 -0400

    remove placeholder setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# setup.sh
-
-echo "Pulling embedding model from Ollama..."
-ollama pull nomic-embed-text
-
-echo "Place your 'stories.json' dataset in this directory before running training."
